bd4dc72ee900c72b0a7693366a20da93c5b02ccf
Norman <takerheil@yahoo.de>
2017-01-11 15:06:22 +0100

    Almost done

--- Check.py
+++ Check.py
@@ -1,1 +1,9 @@
-def Check()
+'''Checks wether rules have to be applied'''
+def Check(Master,Rule_set):
+    Trash=Master['Trash']
+    for i in Rule_set:
+        if Trash[0][0]==i[0]:
+            Rule=i[1].split(',')
+
+        elif i[0]=='':
+            return -1
